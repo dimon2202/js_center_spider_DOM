a3135d39f12825057033a99533843a69a6d79d7e
Dima Sidenko <sidenkodima2202@gmail.com>
2026-01-02 19:31:03 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const way = (wall.clientWidth - spider.clientWidth) / 2;
+
+spider.style.left = way + 'px';
+spider.style.top = way + 'px';
